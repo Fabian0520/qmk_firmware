7d2c32d76a9fb324e4f4834e5d247c4cea08acd0
Fabian Maisberger <fabian.maisberger@googlemail.com>
2020-03-23 09:51:18 +0100

    changed keypap

--- keyboards/crkbd/keymaps/fabian0520/keymap.c
+++ keyboards/crkbd/keymaps/fabian0520/keymap.c
@@ -60,10 +60,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  ), 
 
 [_ADJUST] =LAYOUT(
-  SCRE_UP ,  DF(_QWERTY)  ,  DF(_COLEMAK) ,  DE_KB   ,   ACT_KB  ,   KC_NO  ,  RGB_HUI ,  RGB_SAI ,  RGB_VAI ,  RGB_SPI ,   KC_NO  ,  KC_VOLU ,
-  SCRE_DN , RGB_TOG  ,  RGB_MOD ,  KC_NO   ,   KC_NO  ,   KC_NO  ,  RGB_HUD ,  RGB_SAD ,  RGB_VAD ,  RGB_SPD ,   KC_NO  ,  KC_VOLD ,
-   KC_NO  , RGB_RMOD ,   KC_NO  ,  KC_NO   ,   KC_NO  ,   KC_NO  ,   KC_NO  ,   KC_NO  ,   KC_NO  ,   KC_NO  ,   KC_NO  ,  KC_MUTE ,
-                                   KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS 
+  SCRE_UP ,  DF(_QWERTY)  ,  DF(_COLEMAK) ,  DE_KB   ,   ACT_KB ,   KC_NO  ,  RGB_HUI ,  RGB_SAI ,  RGB_VAI ,  RGB_SPI ,   KC_NO  ,  KC_VOLU ,
+  SCRE_DN ,    RGB_TOG    ,     RGB_MOD   ,  KC_NO   ,   KC_NO  ,   KC_NO  ,  RGB_HUD ,  RGB_SAD ,  RGB_VAD ,  RGB_SPD ,   KC_NO  ,  KC_VOLD ,
+   KC_NO  ,    KC_NO      ,     RGB_RMOD  ,  KC_NO   ,   KC_NO  ,   KC_NO  ,   KC_NO  ,   KC_NO  ,   KC_NO  ,   KC_NO  ,   KC_NO  ,  KC_MUTE ,
+                                  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS 
  ) 
 };
 
