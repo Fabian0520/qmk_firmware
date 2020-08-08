212d0d03b2b21f8463091ad59646e6936954e50b
Fabian Maisberger <fabian.maisberger@googlemail.com>
2020-08-08 13:47:47 +0200

    working keymap. QWERTY has to be done yet.

--- keyboards/the_royal/45_ats/keymaps/fabian0520/keymap.c
+++ keyboards/the_royal/45_ats/keymaps/fabian0520/keymap.c
@@ -19,26 +19,12 @@
 //extern keymap_config_t keymap_config;
 #include QMK_KEYBOARD_H
 
-// Layer names
-enum{
-  // - Base layer:
-  _BASE,
-  // - Symbols, numbers, and functions:
-  _FN,
-  // - Alternate Function layer:
-  _LN,
-    // - HYPE-BOI MLG BBY! layer:
-  _GAM
-};
-
-#define XXXXXX KC_TRNS
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Default QWERTY layer 
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐   ┌───┐
-     * │Esc│ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │Del│BkS│   │PgU│
-     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤   ├───┤
-     * │Tab │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ' │Enter │   │PgD│
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐   ┌───┐
+     * │Esc│ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │  BkS  │   │PgU│
+     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┤   ├───┤
+     * │Tab │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ' │ DEL  │   │PgD│
      * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┘   ├───┤
      * │LShift│ Z │ X │ C │ V │ B │ N │ M │ , │ . │Sft/│ ┌───┐ │TO3│
      * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │ ↑ │ └───┘
@@ -46,68 +32,102 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └────┴───┴────┴──────────┴────────┴────┴────┘ │ ← │ ↓ │ → │
      *                                               └───┴───┴───┘
      */
-    [_BASE] = LAYOUT_split_space(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, KC_BSPC,    KC_PGUP,
-        KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOTE,   KC_ENTER,       KC_PGDN,
-        KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, RSFT_T(KC_SLSH),        KC_UP,     TO(_GAM),
-        KC_LCTL, KC_LGUI, KC_LALT,            MO(_FN),           KC_SPACE,            KC_RALT,   MO(_LN),  KC_LEFT, KC_DOWN, KC_RIGHT
+    [_COLEMAK] = LAYOUT_split_space(
+        KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,  KC_SCLN,    KC_NO, KC_BSPC,    KC_PGUP,
+        LT(_SYM, KC_TAB),   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,   KC_DEL,       KC_PGDN,
+        KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    MY_COMM,    MY_DOT,   KC_RSFT ,     KC_UP,     KC_END,
+        KC_LCTL, KC_LGUI, KC_LALT,            NUM_SPC,           MOV_ENT,            KC_RALT,   KC_RGUI,  KC_LEFT, KC_DOWN, KC_RIGHT
     ),
-    /* Main Numbers, Symbols & Function Layer (MOMENTARY)
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐   ┌───┐
-     * │   │ ! │ @ │ # │ $ │ % │ ^ │ & │ * │ [ │ ] │ \ │   │   │Hme│
-     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤   ├───┤
-     * │    │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │      │   │End│
+    /* Default QWERTY layer 
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐   ┌───┐
+     * │Esc│ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │  BkS  │   │PgU│
+     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┤   ├───┤
+     * │Tab │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ' │ DEL  │   │PgD│
      * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┘   ├───┤
-     * │      │ ( │ ) │ ; │ . │   │ - │ + │ * │ / │ =  │ ┌───┐ │   │
+     * │LShift│ Z │ X │ C │ V │ B │ N │ M │ , │ . │Sft/│ ┌───┐ │TO3│
+     * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │ ↑ │ └───┘
+     * │LCtl│OS │LAlt│    Fn    │  Space │RAlt│ Ln │ ┌───┤───├───┐
+     * └────┴───┴────┴──────────┴────────┴────┴────┘ │ ← │ ↓ │ → │
+     *                                               └───┴───┴───┘
+     */
+    [_QWERTY] = LAYOUT_split_space(
+        KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,  KC_SCLN,    KC_NO, KC_BSPC,    KC_PGUP,
+        LT(_SYM, KC_TAB),   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,   KC_DEL,       KC_PGDN,
+        KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    MY_COMM,    MY_DOT,   KC_RSFT ,     KC_UP,     KC_END,
+        KC_LCTL, KC_LGUI, KC_LALT,            NUM_SPC,           MOV_ENT,            KC_RALT,  KC_RGUI,  KC_LEFT, KC_DOWN, KC_RIGHT
+    ),
+    /* Numbers Layer
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐   ┌───┐
+     * │   │ € │ § │ ° │ ' │   │ / │ 7 │ 8 │ 9 │ * │ \     │   │Hme│
+     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┤   ├───┤
+     * │    │ & │ ^ │ % │ $ │ _ │ . │ 4 │ 5 │ 6 │ + │ ???  │   │End│
+     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┘   ├───┤
+     * │      │ ? │ # │ @ │ ! │ = │ 0 │ 1 │ 2 │ 3 │ -  │ ┌───┐ │   │
      * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │   │ └───┘
      * │    │   │    │          │        │    │    │ ┌───┤───├───┐
      * └────┴───┴────┴──────────┴────────┴────┴────┘ │   │   │   │
      *                                               └───┴───┴───┘
      */
-    [_FN] = LAYOUT_split_space(
-        XXXXXX,  KC_EXLM,    KC_AT,    KC_HASH,    KC_DOLLAR,    KC_PERCENT,   KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LBRC,    KC_RBRC,    KC_BSLS, XXXXXX,    KC_HOME,
-        XXXXXX,   KC_1,        KC_2,     KC_3,       KC_4,       KC_5,      KC_6,       KC_7,       KC_8,       KC_9,       KC_0,          KC_ENTER,       KC_END,
-        XXXXXX,     KC_LPRN,    KC_RPRN,    KC_SCLN,    KC_DOT,    XXXXXX,  KC_MINS,     KC_PLUS,   KC_ASTR,  KC_SLSH,   KC_EQL,              XXXXXX,       KC_NO,
-        XXXXXX,             XXXXXX,         XXXXXX,            XXXXXX,                      XXXXXX,           XXXXXX,   XXXXXX,       XXXXXX, XXXXXX, XXXXXX
+    [_NUM] = LAYOUT_split_space(
+        KC_TRNS,  MY_EURO , MY_PARA, MY_CIRC, KC_QUOT,   KC_NO  , KC_SLASH, KC_7, KC_8, KC_9, KC_ASTR, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  S(KC_7) , S(KC_6), S(KC_5), S(KC_4),S(KC_MINS), KC_COMM , KC_4, KC_5, KC_6, KC_PLUS, KC_TRNS, KC_TRNS,
+        KC_TRNS,S(KC_SLSH), S(KC_3), S(KC_2), S(KC_1), KC_EQUAL ,   KC_0  , KC_1, KC_2, KC_3,KC_MINUS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
-
-    /* ALTERNATE Function layer (MOMENTARY)
-     * ┌────┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-     * │ PWR│F1 │F2 │F3 │F4 │F5 │F6 │   │   │   │   │PRV│NXT│   │VL+│
-     * ├────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┼───┼───┤
-     * │ SLP │F7 │F8 │F9 │F10│F11│F12│   │   │   │   │ PLAY │   │VL-│
-     * ├─────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┴───│───│
-     * │ WAKE  │   │   │   │   │   │   │   │   │   │    │ ┌───┐ │   │
-     * ├────┬──┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │   │ └───┘
-     * │RSET│    │    │          │        │    │    │ ┌───│───│───┐
-     * └────┴────┴────┴──────────┴────────┴────┴────┘ │   │   │   │
-     *                                                └───┴───┴───┘
-     */
-    [_LN] = LAYOUT_split_space(
-        KC_PWR,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    KC_MPRV, KC_MNXT,    KC_VOLU,
-        KC_SLEP,   KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,   KC_MPLY,         KC_VOLD,
-        KC_WAKE,     XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX,    XXXXXX, XXXXXX, XXXXXX,        XXXXXX,     XXXXXX,
-        RESET,      XXXXXX,     XXXXXX,             XXXXXX,             XXXXXX,            XXXXXX,   XXXXXX,           XXXXXX, XXXXXX, XXXXXX
-    ),
-
-    /* GAME HYPE MLG BBY Layer (TOGGLE)
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-     * │Esc│ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │Del│BkS│   │INS│
-     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┼───┼───┤
-     * │Tab │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │Enter │   │PgD│
-     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┴───│───│
-     * │LShift│ Z │ X │ C │ V │ B │ N │ M │ , │ . │Sft/│ ┌───┐ │LY0│
-     * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │ ↑ │ └───┘
-     * │LCtl│OS │LAlt│   Space  │  Space │ Fn │ Ln │ ┌───│───│───┐
-     * └────┴───┴────┴──────────┴─────────────┴────┘ │ ← │ ↓ │ → │
+    /* Symbols Layer
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐   ┌───┐
+     * │   │F12│F11│F10│F9 │   │ + │ [ │ ] │ < │ > │  Enf  │   │Hme│
+     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┤   ├───┤
+     * │    │F8 │F7 │F6 │F5 │   │ - │ ( │ ) │ { │ } │      │   │End│
+     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┘   ├───┤
+     * │      │F4 │F3 │F2 │F1 │   │ = │ | │ \ │ ? │ `  │ ┌───┐ │   │
+     * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │   │ └───┘
+     * │    │   │    │          │        │    │    │ ┌───┤───├───┐
+     * └────┴───┴────┴──────────┴────────┴────┴────┘ │   │   │   │
      *                                               └───┴───┴───┘
      */
-    [_GAM] = LAYOUT_split_space(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, KC_BSPC,    KC_INSERT,
-        KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOTE,   KC_ENTER,       KC_PGUP,
-        KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, RSFT_T(KC_SLSH),        KC_UP,     TO(_BASE),
-        KC_LCTL, KC_LGUI, KC_LALT,            KC_SPACE,           KC_SPACE,            MO(_FN),   MO(_LN),  KC_LEFT, KC_DOWN, KC_RIGHT
-    )
+    [_SYM] = LAYOUT_split_space(
+        KC_TRNS,  KC_F12, KC_F11, KC_F10, KC_F9 , KC_NO, KC_PLUS , KC_LBRC, KC_RBRC, KC_LT  , KC_GT  , KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,  KC_F8 , KC_F7 , KC_F6 , KC_F5 , KC_NO, KC_MINUS, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_DEL , KC_TRNS,
+        KC_TRNS,  KC_F4 , KC_F3 , KC_F2 , KC_F1 , KC_NO, KC_EQUAL, KC_1, KC_2, KC_3,KC_MINUS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    /* Movement Layer
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐   ┌───┐
+     * │   │PgU│HME│ UP│END│   │   │i7 │i8 │i9 │   │       │   │Hme│
+     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┤   ├───┤
+     * │    │PgD│RGT│DWN│LFT│   │   │i4 │i5 │i6 │   │ ???  │   │End│
+     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┘   ├───┤
+     * │      │   │   │   │   │   │i10│i1 │i2 │i3 │    │ ┌───┐ │   │
+     * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │   │ └───┘
+     * │    │   │    │          │        │    │    │ ┌───┤───├───┐
+     * └────┴───┴────┴──────────┴────────┴────┴────┘ │   │   │   │
+     *                                               └───┴───┴───┘
+     */
+    [_MOV] = LAYOUT_split_space(
+        KC_TRNS, KC_PGUP, KC_HOME, KC_UP  , KC_END , KC_NO  , KC_NO  , i3_S_7 , i3_S_8 , i3_S_9 , KC_NO  , KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO  , KC_NO  , i3_S_4 , i3_S_5 , i3_S_6 , KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , i3_S_1 , i3_S_2 , i3_S_3 , KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
+    /* Adjust Layer
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐   ┌───┐
+     * │Br+│QWE│COL│STR│   │   │   │   │   │   │   │       │   │   │
+     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┤   ├───┤
+     * │Br- │UCM│GAM│   │   │   │   │   │   │   │   │      │   │   │
+     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┘   ├───┤
+     * │      │   │   │   │   │   │   │   │   │   │    │ ┌───┐ │   │
+     * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │   │ └───┘
+     * │    │   │    │          │        │    │    │ ┌───┤───├───┐
+     * └────┴───┴────┴──────────┴────────┴────┴────┘ │   │   │   │
+     *                                               └───┴───┴───┘
+     */
+    [_ADJUST] = LAYOUT_split_space(
+        SCRE_UP, DF(_QWERTY), DF(_COLEMAK), X(STAR), KC_NO , KC_NO  , KC_NO  , KC_NO , KC_NO , KC_NO , KC_NO  , KC_TRNS, KC_TRNS, RESET,
+        SCRE_DN, UC_MOD     , DF(_GAME)   , KC_NO  , KC_NO , KC_NO  , KC_NO  , KC_NO , KC_NO , KC_NO , KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_NO      , KC_NO       , KC_NO  , KC_NO , KC_NO  , KC_NO  , KC_NO , KC_NO , KC_NO , KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS    , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
 };
 
     /* Default COLEMAK layer 
@@ -120,54 +140,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │ ↑ │ └───┘
      * │LCtl│OS │LAlt│  Space   │  Enter │RAlt│ OS │ ┌───┤───├───┐
      * └────┴───┴────┴──────────┴────────┴────┴────┘ │ ← │ ↓ │ → │
-     *                                               └───┴───┴───┘
-     */
-    /* Main Numbers, Symbols & Function Layer (MOMENTARY)
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐   ┌───┐
-     * │   │F12│F11│F10│F9 │   │ + │ [ │ ] │ < │ > │  Enf  │   │Hme│
-     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┤   ├───┤
-     * │    │F8 │F7 │F6 │F5 │   │ - │ ( │ ) │ { │ } │      │   │End│
-     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┘   ├───┤
-     * │      │F4 │F3 │F2 │F1 │   │ = │ | │ \ │ ? │ `  │ ┌───┐ │   │
-     * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │   │ └───┘
-     * │    │   │    │          │        │    │    │ ┌───┤───├───┐
-     * └────┴───┴────┴──────────┴────────┴────┴────┘ │   │   │   │
-     *                                               └───┴───┴───┘
-     */
-    /* Main Numbers, Symbols & Function Layer (MOMENTARY)
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐   ┌───┐
-     * │   │ € │ § │ ° │ ' │   │ / │ 7 │ 8 │ 9 │ * │ \     │   │Hme│
-     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┤   ├───┤
-     * │    │ & │ ^ │ % │ $ │ _ │ . │ 4 │ 5 │ 6 │ + │ ???  │   │End│
-     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┘   ├───┤
-     * │      │ ? │ # │ @ │ ! │ = │ 0 │ 1 │ 2 │ 3 │ -  │ ┌───┐ │   │
-     * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │   │ └───┘
-     * │    │   │    │          │        │    │    │ ┌───┤───├───┐
-     * └────┴───┴────┴──────────┴────────┴────┴────┘ │   │   │   │
-     *                                               └───┴───┴───┘
-     */
-    /* Main Numbers, Symbols & Function Layer (MOMENTARY)
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐   ┌───┐
-     * │   │PgU│HME│ UP│END│   │   │i7 │i8 │i9 │   │       │   │Hme│
-     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┤   ├───┤
-     * │    │PgD│RGT│DWN│LFT│   │   │i4 │i5 │i6 │   │ ???  │   │End│
-     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┘   ├───┤
-     * │      │   │   │   │   │   │i10│i1 │i2 │i3 │    │ ┌───┐ │   │
-     * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │   │ └───┘
-     * │    │   │    │          │        │    │    │ ┌───┤───├───┐
-     * └────┴───┴────┴──────────┴────────┴────┴────┘ │   │   │   │
-     *                                               └───┴───┴───┘
-     */
-    /* Main Numbers, Symbols & Function Layer (MOMENTARY)
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐   ┌───┐
-     * │Br+│QWE│COL│STR│   │   │RGB│RGB│RGB│VUP│   │       │   │Hme│
-     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──────┤   ├───┤
-     * │Br- │UCM│GAM│   │   │   │RGB│RGB│RGB│VDW│   │ ???  │   │End│
-     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴──┬───┘   ├───┤
-     * │      │   │   │   │   │   │RGB│RGB│RGB│MUT│    │ ┌───┐ │   │
-     * ├────┬─┴─┬─┴──┬┴───┴───┴─┬─┴───┴──┬┴───│───┴┬───┘ │   │ └───┘
-     * │    │   │    │          │        │    │    │ ┌───┤───├───┐
-     * └────┴───┴────┴──────────┴────────┴────┴────┘ │   │   │   │
      *                                               └───┴───┴───┘
      */
 
@@ -186,32 +158,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //function for layer indicator LED
 uint32_t layer_state_set_keymap(uint32_t state)
 {
-    if (biton32(state) == 1) {
-    writePinHigh(D0);
-	} else {
-		writePinLow(D0);
-    }
-    if (biton32(state) == 2) {
-    writePinHigh(D1);
-	} else {
-		writePinLow(D1);
-    }
-    if (biton32(state) == 3) {
-    writePinHigh(D2);
-	} else {
-		writePinLow(D2);
+    switch(get_highest_layer(default_layer_state)){
+        case _COLEMAK:
+            writePinHigh(D0);
+            writePinLow(D1);
+            writePinLow(D2);
+            break;
+        case _QWERTY:
+            writePinLow(D0);
+            writePinHigh(D1);
+            writePinLow(D2);
+            break;
     }
     return state;
 }
-
-/*
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-}
-*/
