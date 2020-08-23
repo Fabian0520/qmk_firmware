1bf88a00da647de2fdfc3f83c12d657338b4dbb7
Fabian Maisberger <fabian.maisberger@googlemail.com>
2020-08-23 09:20:23 +0200

    added qwerty and adjust layer to keymap

--- keyboards/primekb/prime_e/keymaps/fabian0520/keymap.c
+++ keyboards/primekb/prime_e/keymaps/fabian0520/keymap.c
@@ -25,6 +25,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL,  KC_LGUI,  KC_LALT,        NUM_SPC,	          MOV_ENT , KC_RALT,                          KC_RGUI, KC_RCTL
     ),
 
+    [_QWERTY]=LAYOUT(
+		KC_ESC,   KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,          KC_Y,   KC_U,     KC_I, KC_O,    KC_P,   KC_SCLN, KC_BSPC,
+		LT(_SYM, KC_TAB),   KC_A,     KC_S,     KC_D,   KC_F,    KC_G,          KC_H,   KC_J,     KC_K, KC_L,    KC_SCLN,   KC_DEL,
+		KC_LSFT,  KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,          RESET,  KC_N,     KC_M, MY_COMM, MY_DOT,  KC_RSFT, KC_LSFT,
+		KC_LCTL,  KC_LGUI,  KC_LALT,        NUM_SPC,	          MOV_ENT , KC_RALT,                          KC_RGUI, KC_RCTL
+    ),
+
     [_NUM]=LAYOUT(
 		KC_TRNS,   MY_EURO,   MY_PARA,   MY_CIRC,   KC_QUOT,  KC_NO    ,         KC_SLASH,   KC_7   ,    KC_8   ,     KC_9  ,  KC_ASTR,   KC_TRNS,   KC_TRNS,
 		KC_TRNS,   S(KC_7),   S(KC_6),   S(KC_5),   S(KC_4), S(KC_MINS),          KC_COMM,   KC_4   ,    KC_5   ,     KC_6  ,  KC_PLUS,   KC_TRNS,
@@ -43,6 +50,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		RESET  ,  KC_F12,   KC_F11,   KC_F10,   KC_F9,   KC_NO,          KC_PLUS,   KC_LBRC,  KC_RBRC, KC_LT  , KC_GT  ,    KC_TRNS,  KC_TRNS,
 		KC_TRNS,  KC_F8 ,   KC_F7 ,   KC_F6 ,   KC_F5,   KC_NO,          KC_MINS,   KC_LPRN,  KC_RPRN, KC_LCBR, KC_RCBR,    KC_TRNS,
 		KC_TRNS,  KC_F4 ,   KC_F3 ,   KC_F2 ,   KC_F1,   KC_NO,          KC_TRNS,  KC_EQUAL,   KC_PIPE,  KC_BSLS, KC_QUES, KC_GRAVE,   KC_TRNS,
+		KC_TRNS,  KC_TRNS,                         KC_TRNS,    KC_TRNS,	         KC_TRNS,   KC_TRNS,                        KC_TRNS,  KC_TRNS
+    ),
+
+    [_ADJUST]=LAYOUT(
+		SCRE_UP,  DF(_QWERTY),   DF(_COLEMAK),   X(STAR),   KC_NO,   KC_NO,          KC_NO,   KC_NO,  KC_NO, KC_NO  , KC_NO  ,    KC_NO,  KC_NO,
+		SCRE_DN,  UC_MOD ,   KC_NO ,   KC_NO ,   KC_NO,   KC_NO,          KC_NO,   KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO,
+		KC_NO,  KC_NO ,   KC_NO ,   KC_NO ,   KC_NO,   KC_NO,          KC_NO,  KC_NO,   KC_NO,  KC_NO, KC_NO, KC_NO,   KC_NO,
 		KC_TRNS,  KC_TRNS,                         KC_TRNS,    KC_TRNS,	         KC_TRNS,   KC_TRNS,                        KC_TRNS,  KC_TRNS
     )
 };
@@ -65,6 +79,11 @@ uint32_t layer_state_set_keymap(layer_state_t state)
     switch(get_highest_layer(state)){
         case _COLEMAK:
             writePinHigh(B1);
+            writePinLow(B2);
+            writePinLow(B3);
+            break;
+        case _QWERTY:
+            writePinLow(B1);
             writePinLow(B2);
             writePinLow(B3);
             break;
